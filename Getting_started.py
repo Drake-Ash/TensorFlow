bed22c2bb5d59c5463040dd2e2d670bd5319b266
Drake-Ash <ashwinv4444@gmail.com>
2017-07-27 18:00:38 +0530

    started with getting started

--- Getting_started.py
+++ Getting_started.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
-a = tf.variable(2.0)
-b = tf.variable(3.0)
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
 tf.global_variables_initializer()
+node_adder = a+b
+session = tf.Session()
+print(session.run(node_adder,{a:4,b:5}))
