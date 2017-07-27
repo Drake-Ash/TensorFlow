fe42d297910e23eb22f1b90dc9919635c8458369
Drake-Ash <ashwinv4444@gmail.com>
2017-07-27 18:25:19 +0530

    started with getting started

--- Getting_started.py
+++ Getting_started.py
@@ -1,7 +1,13 @@
 import tensorflow as tf
-a = tf.placeholder(tf.float32)
-b = tf.placeholder(tf.float32)
-tf.global_variables_initializer()
-node_adder = a+b
-session = tf.Session()
-print(session.run(node_adder,{a:4,b:5}))
+a = tf.Variable([3], dtype=tf.float32)
+b = tf.Variable([4], dtype=tf.float32)
+x = tf.placeholder(tf.float32)
+y = tf.placeholder(tf.float32)
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+linear_model = x*a + b
+print(sess.run(linear_model, {x: [1, 2, 3, 4, 8], y: [1, 5, 4, 8, 6]}))
+J = tf.square(linear_model-y)
+J = tf.reduce_sum(J)
+print(sess.run(J, {x: [1, 2, 3, 4, 8], y: [1, 5, 4, 8, 6]}))
