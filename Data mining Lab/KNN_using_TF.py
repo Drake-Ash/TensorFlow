307308572a814ee44e8f9a4ceb3d5ddfc529c2f7
Drake-Ash <ashwinv4444@gmail.com>
2017-08-02 23:50:37 +0530

    Modified KNN using TF, added data file using csv for reading data

--- Data mining Lab/KNN_using_TF.py
+++ Data mining Lab/KNN_using_TF.py
@@ -1,13 +1,30 @@
 import tensorflow as tf
 import numpy as np
 
-x = tf.placeholder(tf.float32)
-y = tf.placeholder(tf.float32)
+data = []
+data_set = open("KNN_data.csv", "r")
+for addr in data_set:
+     quotes = addr.split(',')
+     fields = [f.strip('"') for f in quotes]
+     data.append([int(f) for f in fields])
 
-k = tf.placeholder(tf.float32)
+x_train = [[0 for x in range(2)] for y in range(len(data))]
+y_train = [0 for x in range(len(data))]
 
-sess = tf.session()
+for i in range(len(data)):
+    x_train[i][0] = data[i][0]
+    x_train[i][1] = data[i][1]
+    y_train[i] = data[i][2]
+
+xtr = tf.placeholder(tf.float32, [None, 2])
+ytr = tf.placeholder(tf.float32, [None])
+xte = tf.placeholder(tf.float32)
+
+K = 3
+
+distance = tf.negative(tf.reduce_sum(tf.abs(xtr,xte),axis=1))
+values, indices = tf.nn.top_k(distance, k=K, sorted=False)
+
+sess = tf.InteractiveSession()
 init = tf.global_variables_initializer()
 sess.run(init)
-
-
